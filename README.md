e3ba3af45567ff4a2e0c2aa9ad09465268f19369
Youn <youn.21@hotmail.com>
2017-09-23 12:02:30 +0700

    first com

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# API2
+
 # myFirstNode
---
+++ config/env/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+        mongoUri: 'mongodb://admin:password@localhost:27017/testDB1',
+       debug: true,
+       sessionSecret: 'dev_secret_key'
+}
